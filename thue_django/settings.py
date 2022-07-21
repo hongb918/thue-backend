2bd49b1aa01965e3495d5d8ac2a0c7b5a8447bfa
hongb918 <hong.b918@gmail.com>
2022-07-21 16:11:02 -0500

    adjust

--- thue_django/settings.py
+++ thue_django/settings.py
@@ -126,7 +126,8 @@ MEDIA_URL ='/images/'
 
 STATICFILES_DIR = [BASE_DIR / 'static']
 
-MEDIA_ROOT = 'static/images'
+MEDIA_ROOT = BASE_DIR /'static/images'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
